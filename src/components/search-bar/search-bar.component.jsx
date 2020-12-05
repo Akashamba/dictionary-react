bf8bb8c454c63ce06a63b8f4cb1903b77500024b
Akash Ambashankar <53367477+Akashamba@users.noreply.github.com>
2020-12-05 14:10:50 +0530

    updated to start using heroku api

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import {withRouter} from 'react-router';
 
-// const handleSubmit = (event, props) => {
-//   event.preventDefault();
-//   // props.history.push(event.target[0].value)
-// }
-
 const SearchBar = (props) => (
   <form onSubmit={(event) => props.history.push(event.target[0].value)}>
     <input type="search" />
