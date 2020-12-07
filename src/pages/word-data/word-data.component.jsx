95c64d8bd57d2210e7cb3eeda051f57d92a85bad
Akash Ambashankar <53367477+Akashamba@users.noreply.github.com>
2020-12-07 17:44:19 +0530

    update.db.json for testing purposes

--- src/pages/word-data/word-data.component.jsx
+++ src/pages/word-data/word-data.component.jsx
@@ -29,10 +29,9 @@ class Result extends React.Component {
     if(this.state.data.error)
         return(<div>{this.state.data.error}</div>)
     if(this.state.data.results)
-      return(<div>
+      return(
         <div>{this.state.data.word}: {this.state.data.results[0].definitions[0]}</div>
-      <div>{JSON.stringify(this.state.data)}</div>
-      </div>)
+     )
     return(<div></div>)
   }
 }
