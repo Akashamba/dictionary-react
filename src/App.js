ddc9a06e4ad109b0e985cecad830d93f1fe2447d
Akash Ambashankar <53367477+Akashamba@users.noreply.github.com>
2020-12-10 23:35:05 +0530

    more 'unused'

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import SearchBar from './components/search-bar/search-bar.component';
 import Result from './pages/word-data/word-data.component';
 import Homepage from "./pages/homepage/homepage.component";
 import Navbar from './components/navbar/navbar.component';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, /*Link*/ } from 'react-router-dom';
 import "./styles.css";
 
 export default function App() {
