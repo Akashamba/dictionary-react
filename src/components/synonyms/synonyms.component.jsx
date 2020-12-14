c7be4f98e7c51d0ac1f097e1eb05d9f7ae6634a2
Akash Ambashankar <53367477+Akashamba@users.noreply.github.com>
2020-12-14 10:30:05 +0530

    removed unused vars

--- src/components/synonyms/synonyms.component.jsx
+++ src/components/synonyms/synonyms.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles} from '@material-ui/core/styles';
 import {Paper, Container} from '@material-ui/core'
 import {Link} from 'react-router-dom'
 
